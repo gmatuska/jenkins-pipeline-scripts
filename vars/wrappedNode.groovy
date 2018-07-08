7e942681e144374d4cd84737eee6b20ac698ad5a
Garrett Matuska <garrett.matuska@gmail.com>
2018-07-08 14:05:01 -0700

    fixed EOF issue with wrappedNode

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,4 +1,7 @@
-url: vars.get("registryUrl", "http://localhost:5000")) {
+def call(Map vars, Closure body=null) {
+  vars = vars ?: [:]
+  node(vars.get("label", null)) {
+    WithDockerRegistry(url: vars.get("registryUrl", "localhost:5000")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
           if (body) { body() }
