93fc1aa69f14226d5fa562a685d2ea0940505e95
Garrett Matuska <garrett.matuska@gmail.com>
2018-07-08 13:53:53 -0700

    simplified wrappedNode for now to get build working in Jenkins.

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,26 +1,6 @@
-def call(Map vars, Closure body=null) {
-  vars = vars ?: [:]
-  node(vars.get("label", null)) {
-    withDockerRegistry(url: vars.get("registryUrl", "https://index.docker.io/v1/"), credentialsId: vars.get("registryCreds", "dockerbuildbot-index.docker.io")) {
+url: vars.get("registryUrl", "http://localhost:5000")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
-          if (vars.get('cleanWorkspace', false)) {
-            // NOTE: `withChownWorkspace` uses docker. if our `label` doesn't have docker
-            // or is misconfigured, these operations will fail and the exception will be
-            // propogated.
-            withChownWorkspace { echo "cleanWorkspace: Ensuring workspace is owned by ${env.USER}" }
-            echo "Removing all docker containers"
-            try {
-              sh("for cid in \$(docker container ls -aq); do docker container rm -vf \$cid; done")
-            } catch (Exception e) {
-              println(e)
-            }
-            echo "Docker containers have been removed"
-
-            echo "cleanWorkspace: Removing existing workspace"
-            deleteDir()
-            echo "cleanWorkspace: Workspace is clean."
-          }
           if (body) { body() }
         }
       }
