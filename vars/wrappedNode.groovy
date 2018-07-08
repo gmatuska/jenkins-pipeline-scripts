0df6a06573174d26a1e44243dfd0b1d7e9617868
Garrett Matuska <garrett.matuska@gmail.com>
2018-07-08 16:21:04 -0700

    changed WithDockerRegistry to withDockerRegistry

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -1,7 +1,7 @@
 def call(Map vars, Closure body=null) {
   vars = vars ?: [:]
   node {
-    WithDockerRegistry(url: vars.get("registryUrl", "localhost:5000")) {
+    withDockerRegistry(url: vars.get("registryUrl", "localhost:5000")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
           if (body) { body() }
