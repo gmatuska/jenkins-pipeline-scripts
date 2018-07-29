bd4b2181fa59601eb9bcee05e878ed0b2cdfef57
Garrett Matuska <garrett.matuska@gmail.com>
2018-07-29 14:04:11 -0700

    adding Clean Workspace functionality back to jenkins scripts

--- vars/wrappedNode.groovy
+++ vars/wrappedNode.groovy
@@ -4,6 +4,22 @@ def call(Map vars, Closure body=null) {
     withDockerRegistry(url: vars.get("registryUrl", "localhost:5000")) {
       wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
+          if (vars.get('cleanWorkspace', false)) {
+            // NOTE: `withChownWorkspace` uses docker. if our `label` doesn't have docker
+            // or is misconfigured, these operations will fail and the exception will be
+            // propogated.
+            withChownWorkspace { echo "cleanWorkspace: Ensuring workspace is owned by ${env.USER}" }
+            echo "Removing all docker containers"
+            try {
+              sh("for cid in \$(docker container ls -aq); do docker container rm -vf \$cid; done")
+            } catch (Exception e) {
+              println(e)
+            }
+            echo "Docker containers have been removed"
+
+            echo "cleanWorkspace: Removing existing workspace"
+            deleteDir()
+            echo "cleanWorkspace: Workspace is clean."
           if (body) { body() }
         }
       }
